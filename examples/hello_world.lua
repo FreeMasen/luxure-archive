e535065bc9d8cd547f00109199819508a6ee4792
rfm <r@robertmasen.pizza>
2020-11-03 20:10:24 -0600

    update example to match readme

--- examples/hello_world.lua
+++ examples/hello_world.lua
@@ -1,13 +1,33 @@
 local socket = require 'socket'
 local lux = require 'luxure'
+local dkjson = require 'dkjson'
 
 local server = lux.Server.new(socket)
+
+server:use(function (req, res, next)
+    local h = req:get_headers()
+    if req.method == 'POST' and h.content_type == 'application/json' then
+        req.raw_body = req:get_body()
+        local success, body = pcall(dkjson.decode, req.raw_body)
+        if success then
+            req.body = body
+        else
+            print('failed to parse json')
+        end
+    end
+    next.fn(req, res, next.next)
+end)
+
 server:get('/', function(req, res)
-    res.send('Hello world!')
+    res:send('Hello world!')
 end)
 
 server:get('/:name', function(req, res)
-    res.send(string.format('Hello %s', req.params.name))
+    res:send(string.format('Hello %s', req.params.name))
+end)
+
+server:post('/', function(req, res)
+    res:send(string.format('Hello %s', req.body.name))
 end)
 
 server:listen(8080)
