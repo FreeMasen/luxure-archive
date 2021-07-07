4cee7f2fb55ff5800776621fcc5b64813e908831
Robert Masen <robert.masen@smartthings.com>
2021-07-07 14:01:48 -0500

    update types in tests

--- spec/router_spec.lua
+++ spec/router_spec.lua
@@ -5,41 +5,54 @@ local MockSocket = require 'spec.mock_socket'.MockSocket
 describe('Router', function ()
     it('Should call callback on route', function()
         local r = Router.new()
-        local req = Request.incoming(MockSocket.new_with_preamble('GET', '/'))
+        local req = Request.tcp_source(MockSocket.new_with_preamble('GET', '/'))
         local called = false
         r:register_handler('/', 'GET', function(req, res)
             called = true
         end)
         r:route(
             req,
-            Response.outgoing(MockSocket.new())
+            Response.new(200, MockSocket.new())
         )
         assert(called)
     end)
     it('should not call if route not matched', function()
         local r = Router.new()
-        local req = Request.incoming(MockSocket.new_with_preamble('GET', '/not_registered'))
+        local req = Request.tcp_source(MockSocket.new_with_preamble('GET', '/not_registered'))
         local called = false
         r:register_handler('/', 'GET', function(req, res)
             called = true
         end)
         assert(not r:route(
             req,
-            Response.outgoing(MockSocket.new())
+            Response.new(200, MockSocket.new())
         ))
         assert(not called)
     end)
     it('should not call if method not matched', function()
         local r = Router.new()
-        local req = Request.incoming(MockSocket.new_with_preamble('POST', '/'))
+        local req = Request.tcp_source(MockSocket.new_with_preamble('POST', '/'))
         local called = false
         r:register_handler('/', 'GET', function(req, res)
             called = true
         end)
         assert(not r:route(
             req,
-            Response.incoming(MockSocket.new())
+            Response.tcp_source(MockSocket.new())
         ))
         assert(not called)
+    end)
+    it('Handler error should panic', function()
+        local r = Router.new()
+        local req = Request.tcp_source(MockSocket.new_with_preamble('GET', '/'))
+        local res = Response.new(200, MockSocket.new())
+        r:register_handler('/', 'GET', function(req, res)
+            error('called')
+        end)
+        assert(not r:route(
+            req,
+            res
+        ))
+        assert.are.equal(500, res.status)
     end)
 end)
